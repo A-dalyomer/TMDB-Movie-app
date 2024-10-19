2091c0ca6d25c26544f7b9f7688af647d15b2f79
ahmad daly <Ahmaddo1999@gmail.com>
2024-10-20 02:41:56 +0300

    Added id to movie summary

--- lib/src/feature/home/data/model/movie_summary_model.dart
+++ lib/src/feature/home/data/model/movie_summary_model.dart
@@ -2,12 +2,14 @@ import '../../domain/entity/movie_summary.dart';
 
 class MovieSummaryModel extends MovieSummary {
   const MovieSummaryModel({
+    required super.id,
     required super.posterImage,
     required super.title,
     required super.rating,
   });
   factory MovieSummaryModel.fromJson(Map<String, dynamic> json) {
     return MovieSummaryModel(
+      id: json["id"],
       posterImage: json['poster_path'] ?? '',
       title: json['original_title'] ?? 'No title',
       rating: json['vote_average'] ?? 0,
@@ -16,6 +18,7 @@ class MovieSummaryModel extends MovieSummary {
 
   factory MovieSummaryModel.fromEntity(MovieSummary movie) {
     return MovieSummaryModel(
+      id: movie.id,
       posterImage: movie.posterImage,
       title: movie.title,
       rating: movie.rating,
--- lib/src/feature/home/domain/entity/movie_summary.dart
+++ lib/src/feature/home/domain/entity/movie_summary.dart
@@ -2,14 +2,16 @@ import 'package:equatable/equatable.dart';
 
 class MovieSummary extends Equatable {
   const MovieSummary({
+    required this.id,
     required this.posterImage,
     required this.title,
     required this.rating,
   });
+  final int id;
   final String posterImage;
   final String title;
   final double rating;
 
   @override
-  List<Object?> get props => [posterImage, title, rating];
+  List<Object?> get props => [id, posterImage, title, rating];
 }
