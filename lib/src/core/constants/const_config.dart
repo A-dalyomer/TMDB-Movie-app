dd92f8f92603063b62876d59bf21a4d1a2adca7a
ahmad daly <Ahmaddo1999@gmail.com>
2024-10-20 02:40:20 +0300

    Created core constant configs

---
+++ lib/src/core/constants/const_config.dart
@@ -0,0 +1,11 @@
+class ConstConfig {
+  static int cacheListSize = 20;
+
+  static String imagesBase(String imagePath) {
+    return "https://image.tmdb.org/t/p/w500$imagePath";
+  }
+
+  static String posterVideoBase(int videoKey) {
+    return "https://www.youtube.com/watch?v=$videoKey";
+  }
+}
