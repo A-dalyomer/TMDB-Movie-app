6d7e9fc9b31bc9e71695a5f98bafb6e697d173ff
ahmad daly <Ahmaddo1999@gmail.com>
2024-10-20 11:53:36 +0300

    Initial localization support for future improvements

---
+++ lib/src/feature/localization/domain/util/app_localizations.dart
@@ -0,0 +1,6 @@
+class AppLocalizations {
+  static const String nowPlaying = "Now Playing";
+  static const String popular = "Popular";
+  static const String topRated = "Top Rated";
+  static const String movies = "Movies";
+}
---
+++ lib/src/feature/localization/domain/util/locale_manager.dart
@@ -0,0 +1,5 @@
+class LocaleManager {
+  static tr(String word) {
+    return word;
+  }
+}
