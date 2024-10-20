c729201eebddec64795e4b44aab4706c533e32e4
ahmad daly <Ahmaddo1999@gmail.com>
2024-10-20 23:01:35 +0300

    Loading error widget UI changes

--- lib/src/core/widget/loading_error_widget.dart
+++ lib/src/core/widget/loading_error_widget.dart
@@ -7,14 +7,24 @@ class LoadingErrorWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(title ?? "Error"),
-        ElevatedButton(
-          onPressed: refresh,
-          child: const Text("Retry"),
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              title ?? "Error Loading data, Please try again later",
+              style: Theme.of(context).textTheme.titleMedium,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: refresh,
+            child: const Text("Retry"),
+          ),
+        ],
+      ),
     );
   }
 }
