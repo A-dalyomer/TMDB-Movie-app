a82879b61b69086da614d011081e47bd2bc0f965
ahmad daly <Ahmaddo1999@gmail.com>
2024-10-20 23:46:35 +0300

    UI adaptations for movies list screen

--- lib/src/feature/home/presentation/screen/movies_list_screen.dart
+++ lib/src/feature/home/presentation/screen/movies_list_screen.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tmdp_movie_app/src/feature/home/domain/entity/movie_summary.dart';
+import 'package:tmdp_movie_app/src/feature/home/domain/util/enum.dart';
 import 'package:tmdp_movie_app/src/feature/home/presentation/bloc/movies_home_bloc.dart';
 import 'package:tmdp_movie_app/src/feature/home/presentation/widget/movie_item.dart';
-import 'package:tmdp_movie_app/src/feature/localization/domain/util/app_localizations.dart';
 import 'package:tmdp_movie_app/src/feature/localization/domain/util/locale_manager.dart';
 
 class MoviesListScreen extends StatelessWidget {
-  const MoviesListScreen({super.key, required this.movies});
+  const MoviesListScreen({
+    super.key,
+    required this.movies,
+    required this.movieType,
+  });
   final List<MovieSummary> movies;
+  final MovieType movieType;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(LocaleManager.tr(AppLocalizations.movies)),
+        title: Text(LocaleManager.tr(movieType.name)),
       ),
       body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 3,
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisExtent: MediaQuery.sizeOf(context).height * 0.25,
+          crossAxisSpacing: 12,
+          mainAxisSpacing: 6,
         ),
+        itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
           return MovieItem(
