f2ba3b713007a02009f88d6bbe6e460d22746f1f
ahmad daly <Ahmaddo1999@gmail.com>
2024-10-21 03:12:36 +0300

    App theme card color

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        cardColor: Colors.purple,
       ),
       home: MoviesHomeScreen(
         moviesRepository: DIManager.getIt<MoviesRepository>(),
