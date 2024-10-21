0a0e587bdc37ffdc79745ac3619d2f316a4889a8
ahmad daly <Ahmaddo1999@gmail.com>
2024-10-21 03:46:04 +0300

    Implemented theme mode in app flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:tmdp_movie_app/src/core/constants/const_config.dart';
 import 'package:tmdp_movie_app/src/core/di/di_manager.dart';
 import 'package:tmdp_movie_app/src/feature/home/domain/repository/movies_repository.dart';
 import 'package:tmdp_movie_app/src/feature/home/presentation/screen/movies_home_screen.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:tmdp_movie_app/src/feature/theme/presentation/bloc/theme_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,16 +26,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: ConstConfig.appName,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        cardColor: Colors.purple,
-      ),
-      home: MoviesHomeScreen(
-        moviesRepository: DIManager.getIt<MoviesRepository>(),
-      ),
+    return BlocProvider<ThemeBloc>(
+      create: (context) => ThemeBloc(),
+      child: Builder(builder: (context) {
+        return BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (context, state) {
+            ThemeMode themeMode() {
+              switch (state) {
+                case ThemeInitial():
+                  return state.mode;
+              }
+            }
+
+            return MaterialApp(
+              title: ConstConfig.appName,
+              themeMode: themeMode(),
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+                cardColor: Colors.purple,
+              ),
+              home: MoviesHomeScreen(
+                moviesRepository: DIManager.getIt<MoviesRepository>(),
+              ),
+            );
+          },
+        );
+      }),
     );
   }
 }
